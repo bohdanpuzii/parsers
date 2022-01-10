7f75e2d07383a5451949f16e228c643d25553f6b
Bohdan Puzii <bogdan.puziy@gmail.com>
2022-01-10 23:43:33 +0200

    massimo parser

--- bershka/bershka.py
+++ bershka/bershka.py
@@ -3,8 +3,9 @@ from bs4 import BeautifulSoup
 raw_description = '<h2>Опис</h2> <div class="model-wrapper"><p class="model">Розмір моделі: 36</p><p class="model">Висота моделі: ' \
       '175 cm</p></div> <p class="description">Колір: Світло-синій</p> <!----> <!---->'
 
-raw_chars = '<h2>Склад</h2> <article class="composition"><h3>Верх</h3> <p>100% поліуретан</p></article><article class="compo' \
-            'sition"><h3>Підкладка</h3> <p>100% поліуретан</p></article><article class="composition"><h3>Підошва</h3> <p>100% поліуретановий термопластик</p></article>'
+raw_chars = '<h2>Склад</h2> <article class="composition"><h3>MAIN FABRIC</h3> <p>86% cotton</p></article><article cla' \
+            'ss="composition"><h3>MAIN FABRIC</h3> <p>14% polyester</p></article><article class="composition"><h3>DET' \
+            'AILS</h3> <p>97% cotton</p></article><article class="composition"><h3>DETAILS</h3> <p>3% elastane</p></article>'
 
 
 def clear_bershka_description_html(raw_characteristics):
@@ -32,5 +33,5 @@ def pretty_bershka_characteristics(chars_list):
 
 
 if __name__ == '__main__':
-    print(clear_bershka_description_html(raw_description))
-    print(pretty_bershka_characteristics(clear_bershka_characteristics_html(raw_chars)))
+    print((clear_bershka_description_html(raw_description)))
+    print((pretty_bershka_characteristics(clear_bershka_characteristics_html(raw_chars))))
