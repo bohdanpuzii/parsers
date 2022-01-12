bd78b68c17b781ab65d0322a15428eef98a92dec
Bohdan Puzii <bogdan.puziy@gmail.com>
2022-01-12 20:25:48 +0200

    update bershka parser

--- bershka/bershka.py
+++ bershka/bershka.py
@@ -1,7 +1,9 @@
 from bs4 import BeautifulSoup
 
-raw_description_html = '<h2>Опис</h2> <div class="model-wrapper"><p class="model"> Розмір моделі: S </p><p class="model"> ' \
-                       'Висота моделі: 177 cm </p></div> <p class="description"> Колір: Зелений </p> <!----> <!---->'
+raw_description_html = '<h2>Опис</h2> <div class="model-wrapper"></div> <p class="description"> Колір: Коричневий </p> ' \
+                       '<p class="description"> Висота підборів: 8&nbsp;см. AIRFIT®. УСТІЛКА З ГНУЧКОЇ ТЕХНІЧНОЇ ЛАТЕКСНОЇ ' \
+                       'ПІНИ ДЛЯ ЗАБЕЗПЕЧЕННЯ БІЛЬШОГО КОМФОРТУ. </p> <p class="description"> Brown platform heel ankle boots.' \
+                       ' Made of stretch fabric. Pointed toe. </p>'
 
 raw_characteristics_html = '<h2>Склад</h2> <article class="composition"><h3>Зовнішня сторона</h3> <p>\n 100% поліестер\n ' \
                            '</p></article><article class="composition"><h3>Підкладка</h3> <p>\n 100% поліестер\n </p>' \
@@ -13,7 +15,7 @@ def clear_bershka_description_html(raw_description):
     soup = BeautifulSoup(raw_description, 'html.parser')
     for p in soup.select('p'):
         characteristics_list.append(p.text.replace('\n', ''))
-    return '\n'.join(list(map(lambda string: string.strip(' ').replace(' ', ''), characteristics_list)))
+    return '\n'.join(list(map(lambda string: string.strip(' '), characteristics_list)))
 
 
 def clear_bershka_characteristics_html(raw_characteristics):
